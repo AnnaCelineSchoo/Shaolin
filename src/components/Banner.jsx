2df01cac8fac2eb0965d64bf488f5f4167ff603c
AnnaCelineSchoo <131945624+AnnaCelineSchoo@users.noreply.github.com>
2024-11-09 15:15:10 +0100

    Revert "Updates"
    This reverts commit b42d78aa892ad8fb710a3d7fafcf74c5815bf2f2.

---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h1 className="banner-title">Shaolin Workout</h1>
+        <p className="banner-subtitle">
+          Achieve your fitness goals with precision and dedication through our structured workout plans.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/pushupsTable.jsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from "react";
+
+function PushupTable({week}){
+    const [data, setData] = useState([]);
+    const [excersises, setExcersises] = useState([]);
+
+    useEffect(() => {
+        // Simulate data fetching
+         const fetchData = async () => {
+            try {
+                const response = await fetch(`${process.env.PUBLIC_URL}/workouts/pushup.json`);
+                //const response = await fetch('/workouts/pushup.json');
+                const result = await response.json();
+                setData(result);
+                console.log("fetched data", result); // Log only once on mount
+
+                if (result.weeks && result.weeks[week]) {
+                    const pushupWeekExcersises = data.weeks[week]; // Ensure workoutData is defined
+                    setExcersises(pushupWeekExcersises);
+                    console.log("Workout data for type:", pushupWeekExcersises);
+                }else{
+                    console.warn(`No data found for week: ${week}`);
+                    setExcersises([]); // Clear exercises if data is not found
+                }
+                
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setExcersises([]);
+            }
+        };
+
+        fetchData();
+    }, [week]);
+
+        return (
+        <div>
+            <h4 className="bg-black text-white text-center py-2">Pushups</h4>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Round</th>
+                        <th>reps</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {excersises.map((excersise, index) =>{
+                        return (
+                        <tr key={index}>
+                            <th>{index+1}</th>
+                            <td>{excersise}</td>
+                        </tr>
+                        )
+                    })}
+                </tbody>
+            </table>  
+        </div>
+    );           
+}
+
+export default PushupTable;
